83462d5249e8b5bc5f6314ff3e01a586028f7b11
Keigo Hashimoto <hashimoto.keigo@moneyforward.co.jp>
2026-04-13 13:40:22 +0900

    Finisih toy app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :microposts
+  validates :name , length:{ maximum:40 },
+                    presence: true
+  validates :email , length:{ maximum:40 }, 
+                    presence: true
 end
